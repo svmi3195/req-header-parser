9080a396ff83c3a9a97df5078d206c788d1629f2
svmi3195 <target1983@gmail.com>
2016-12-26 21:06:27 +0200

    fixed getting ip

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ var http = require('http');
 http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'application/json'});
 
-    var address = req.headers.host || null;
+    var address = req.headers['x-forwarded-for'] || 
+                req.connection.remoteAddress || 
+                req.socket.remoteAddress || 
+                null;
+    if(address && address.indexOf(',') != -1){
+        address = address.slice(0, address.indexOf(','));
+    }
 
     var lang = req.headers['accept-language'] || null;
-    if(lang){
+    if(lang && address.indexOf(',') != -1){
         lang = lang.slice(0, lang.indexOf(','));
     }
     
     var soft = req.headers['user-agent'] || null;
-    if(soft){
+    if(soft && soft.indexOf('(') != -1){
         soft = soft.slice(soft.indexOf('(') + 1, soft.indexOf(')'));
     }    
 
